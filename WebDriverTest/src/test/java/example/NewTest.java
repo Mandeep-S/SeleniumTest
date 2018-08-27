760f50ad8b04fc5886bbb84b7933f61a6ff17f63
Mandeep <Mandeep@192.168.0.18>
2018-08-26 20:03:27 -0400

    Project Committed

---
+++ WebDriverTest/src/test/java/example/NewTest.java
@@ -0,0 +1,32 @@
+package example;		
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+public class NewTest {		
+	    private WebDriver driver;		
+		@Test				
+		public void testEasy() {	
+			driver.get("http://demo.guru99.com/test/guru99home/");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+		}	
+		@BeforeTest
+		public void beforeTest() {	
+			System.setProperty("webdriver.chrome.driver","D:\\Study\\chromedriver_win32\\chromedriver.exe");
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("disable-infobars");
+			options.addArguments("disable-notifications");
+				
+			driver = new ChromeDriver(options);
+			driver.manage().window().maximize();  
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+}	
